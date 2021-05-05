4efd4908d67a9aca11d2fdd561959f5ff22b160a
Aryan760 <81566436+Aryan760@users.noreply.github.com>
2021-05-05 17:35:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var fixedrect,movingrect
+
+function setup() {
+  createCanvas(1200,800);
+  fixedrect=createSprite(400,100,50,80)
+  fixedrect.shapeColor="green"
+  movingrect=createSprite(400,800,80,30)
+  movingrect.shapeColor="green"
+
+}
+
+function draw() {
+  background(0);  
+  movingrect.x=mouseX
+  movingrect.y=mouseY
+  if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2&&fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2&&movingrect.y-fixedrect.y<fixedrect.width/2+movingrect.width/2&&fixedrect.y-movingrect.y<fixedrect.width/2+movingrect.width/2){
+    movingrect.shapeColor="red"
+    fixedrect.shapeColor="red"
+  }
+  else{
+    movingrect.shapeColor="green"
+    fixedrect.shapeColor="green"
+  }
+  drawSprites();
+}
